9858fc12a9c1b639002b01ddd8fe27d90d71c567
伊東和成 <itokazunari5375@icloud.com>
2022-06-14 22:52:38 +0900

    line botと通信できるように設定

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.5'
 
+gem 'dotenv-rails'
+
 gem 'line-bot-api'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.6'
--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -2,5 +2,16 @@ class LineBotController < ApplicationController
   protect_from_forgery except: [:callback]
  
   def callback
+    body = request.body.read
+    p body
+  end
+
+  private
+ 
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
   end
 end
