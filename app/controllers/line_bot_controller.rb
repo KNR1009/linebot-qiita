c5570efac1ce90bf5db3fd097e6dd5795784daf8
伊東和成 <itokazunari5375@icloud.com>
2022-06-14 22:46:13 +0900

    セキュリティー対策全般

---
+++ app/controllers/line_bot_controller.rb
@@ -0,0 +1,6 @@
+class LineBotController < ApplicationController
+  protect_from_forgery except: [:callback]
+ 
+  def callback
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get "hello/index"
+  # LINE BOT用のコールバックルーティン
+  post 'callback' => 'line_bot#callback'
 end
